d8fcd50fb4e890c80f2fd6ba13efc5605ff47b53
dbc2201 <divyanshb22@gmail.com>
2022-02-04 18:10:14 +0530

    Student class ke fields ko initialize karne ke liye constructor banaya hai

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -4,4 +4,10 @@ public class Student {
     private String firstName;
     private String lastName;
     private long rollNumber;
+
+    public Student(String firstName, String lastName, long rollNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.rollNumber = rollNumber;
+    }
 }
