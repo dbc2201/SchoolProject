ee217e034affd9c9c1477942d1b2caa0f52764f8
dbc2201 <divyanshb22@gmail.com>
2022-02-04 18:10:49 +0530

    Student class ke private fields ko acess karne ke liye getter and setter methods banaye hain

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -10,4 +10,28 @@ public class Student {
         this.lastName = lastName;
         this.rollNumber = rollNumber;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(long rollNumber) {
+        this.rollNumber = rollNumber;
+    }
 }
