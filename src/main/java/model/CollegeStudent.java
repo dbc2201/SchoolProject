58868bb26256036d0259f678717576575c99dd84
dbc2201 <divyanshb22@gmail.com>
2022-02-04 18:19:06 +0530

    CollegeStudent ke liye extra fields add kiya hain

--- src/main/java/model/CollegeStudent.java
+++ src/main/java/model/CollegeStudent.java
@@ -1,6 +1,10 @@
 package model;
 
 public class CollegeStudent extends Student {
+
+    private boolean isPlaced; // wo college student place hai ya nahi
+    private String collegeEmailAddress;
+
     public CollegeStudent(String firstName, String lastName, long rollNumber) {
         super(firstName, lastName, rollNumber);
     }
